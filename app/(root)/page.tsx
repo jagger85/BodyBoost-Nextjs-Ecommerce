ca2a9468b6fca6e8b79d48086c1d5a0b57830618
Jagger85 <andresfonsortega@yahoo.es>
2025-01-10 11:24:54 +0700

    Feat(getLatestProducts): - Created a server action to fetch latest products from neon postgre db - Adapted components for using the fetched products from de db

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
-import sampleData from '@/db/sample-data'
 import ProductList from '@/components/shared/product/product-list'
-const Homepage = () => {
+import { getLatestProducts } from '@/lib/actions/product.actions';
+const Homepage = async () => {
+
+    const latestProducts = await getLatestProducts()
+
     return ( 
             <>
-            <ProductList data={sampleData.products} title="Newest Arrivals" limit={4}/>
+            <ProductList data={latestProducts} title="Newest Arrivals" />
             </>
     );
 }
