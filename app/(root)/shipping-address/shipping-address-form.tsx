f5de51740ac9f88cd66dfb586bce2f7479abbb50
Jagger85 <andresfonsortega@yahoo.es>
2025-01-12 15:46:23 +0700

    Feat(shipping-addres): Shipping Addres step completed

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -6,13 +6,17 @@ import { useTransition } from 'react'
 import { shippingAddressSchema } from '@/lib/validators'
 import { ShippingAddress } from '@/types'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { ControllerRenderProps, useForm } from 'react-hook-form'
+import { ControllerRenderProps, useForm, SubmitHandler } from 'react-hook-form'
 import { z } from 'zod'
 import { shippingAddressDefaultValues } from '@/lib/constants'
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { ArrowRight, Loader } from 'lucide-react'
+import { updateUserAddress } from '@/lib/actions/user.actions'
+
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
-  const router = userRouter()
+  const router = useRouter()
   const { toast } = useToast()
 
   const form = useForm<z.infer<typeof shippingAddressSchema>>({
@@ -22,8 +26,19 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
 
   const [isPending, startTransition] = useTransition()
 
-  const onSubmit = () =>{
-    return
+  const onSubmit:SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (values ) =>{
+    
+    startTransition(async () =>{
+      const res = await updateUserAddress(values)
+      if(!res.success){
+        toast({
+          variant:'destructive',
+          description: res.message
+        })
+        return
+      }
+      router.push('/payment-method')
+    })
   }
 
   return (
@@ -63,6 +78,57 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
                 )}
               />
             </div>
+            <div className='flex flex-col md:flex-row gap-5'>
+              <FormField
+                control={form.control}
+                name='city'
+                render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>,'city'>}) => (
+                  <FormItem className='w-full'>
+                    <FormLabel>City</FormLabel>
+                    <FormControl>
+                      <Input placeholder='City' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='flex flex-col md:flex-row gap-5'>
+              <FormField
+                control={form.control}
+                name='postalCode'
+                render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>,'postalCode'>}) => (
+                  <FormItem className='w-full'>
+                    <FormLabel>Postal Code</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Enter postal code' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className='flex flex-col md:flex-row gap-5'>
+              <FormField
+                control={form.control}
+                name='country'
+                render={({ field }: {field: ControllerRenderProps<z.infer<typeof shippingAddressSchema>,'country'>}) => (
+                  <FormItem className='w-full'>
+                    <FormLabel>Country</FormLabel>
+                    <FormControl>
+                      <Input placeholder='Enter country' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          <div className="flex gap-2">
+            <Button type='submit' disabled={isPending}>
+              { isPending ? (<Loader className='w-4 h-4 animate-spin'/>): (<ArrowRight className='w-4 h-4'/>)} 
+              Continue
+            </Button>
+          </div>
           </form>
         </Form>
       </div>
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,75 +1,99 @@
 'use server'
 
-import { signInFormSchema } from "../validators"
-import { signUpFormSchema } from "../validators"
-import { signIn, signOut} from '@/auth'
-import { isRedirectError } from "next/dist/client/components/redirect-error"
+import { shippingAddressSchema, signInFormSchema } from '../validators'
+import { signUpFormSchema } from '../validators'
+import { signIn, signOut } from '@/auth'
+import { isRedirectError } from 'next/dist/client/components/redirect-error'
 import { hash } from '@/lib/encrypt'
 import { prisma } from '@/db/prisma'
-import { formatError } from "../utils"
+import { formatError } from '../utils'
+import { ShippingAddress } from '@/types'
+import { auth } from '@/auth'
+
 // Sign in the user with credentials
-export async function signInWithCredentials(prevState: unknown, formData: FormData){
+export async function signInWithCredentials(prevState: unknown, formData: FormData) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formData.get('email'),
+      password: formData.get('password'),
+    })
 
-    try{
-        const user = signInFormSchema.parse({
-            email: formData.get('email'),
-            password: formData.get('password'),
-        })
-
-        await signIn('credentials', user)
-        return { success: true, message:'Signed in successfully'}
-    } catch (error) {
-        if (isRedirectError(error)){
-            throw error
-        }
-        return { success: false, message: 'Invalid email or password'}
+    await signIn('credentials', user)
+    return { success: true, message: 'Signed in successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
     }
+    return { success: false, message: 'Invalid email or password' }
+  }
 }
 
 // Sign user out
-export async function signOutUser(){
-    await signOut()
+export async function signOutUser() {
+  await signOut()
 }
 
 //Sign up user
-export async function signUpUser(prevState: unknown, formData: FormData){
-    try{
-        const user = signUpFormSchema.parse({
-            name: formData.get('name'),
-            email: formData.get('email'),
-            password: formData.get('password'),
-            confirmPassword: formData.get('confirmPassword'),
-        })
-        const plainPassword = user.password
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    })
+    const plainPassword = user.password
 
-        user.password = await hash(user.password);
+    user.password = await hash(user.password)
 
-        await prisma.user.create({
-            data: {
-                name: user.name,
-                email: user.email,
-                password: user.password
-            }
-        })
-        await signIn('credentials',{
-            email: user.email,
-            password: plainPassword,
-        })
-        return { success: true , message: 'User registered succesfully'}
-    }catch(error){
-
-        if(isRedirectError(error)){
-            throw error
-        }
-        return { success: false , message: formatError(error)}
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    })
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    })
+    return { success: true, message: 'User registered succesfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
     }
+    return { success: false, message: formatError(error) }
+  }
 }
 
 // Get user by the ID
-export async function getUserById(userId: string){
-    const user = await prisma.user.findFirst({
-        where: {id: userId}
+export async function getUserById(userId: string) {
+  const user = await prisma.user.findFirst({
+    where: { id: userId },
+  })
+  if (!user) throw new Error('User not found')
+  return user
+}
+
+// Update the user's address
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth()
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
     })
-    if(!user) throw new Error('User not found')
-    return user
+
+    if (!currentUser) throw new Error('User not found')
+
+    const address = shippingAddressSchema.parse(data)
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address },
+    })
+    return { success: true, message: 'User updated successfully' }
+  } catch (error) {
+    return { success: false, message: formatError(error) }
+  }
 }
