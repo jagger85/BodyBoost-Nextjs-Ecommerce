acf1dc47350f27282ccd667fb98b29769e35ace4
Jagger85 <andresfonsortega@yahoo.es>
2025-01-12 16:10:42 +0700

    Fix(middelware): Updated middelware

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,12 @@
-export { auth as middleware} from '@/auth'
+export { default as middleware } from 'next-auth/middleware'
+
+export const config = {
+  matcher: [
+    // Match all routes that need authentication
+    '/cart',
+    '/shipping-address',
+    '/orders/:path*',
+    // Exclude these routes from middleware
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+}
