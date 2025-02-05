de9ff7463abb6da2aba939fefbe47366fee69d4f
Jagger85 <andresfonsortega@yahoo.es>
2025-02-05 18:23:32 +0700

    Chore(wrapper): Improved wrapper over components

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -3,7 +3,7 @@ import { Product } from "@/types";
 const ProductList = ({data, title, limit}: {data: Product[]; title?: string; limit?: number}) => {
     const limitedData = limit ? data.slice(0, limit) : data
     return (
-        <div className='my-10'>
+        <div className='my-10 wrapper'>
             <h2 className="h2-bold mb-4">{title}</h2>
             {data.length > 0 ? (
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
