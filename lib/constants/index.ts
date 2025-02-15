8dbe72ec19881b74932977b58cfcd014f3f2cd0f
Jagger85 <andresfonsortega@yahoo.es>
2025-02-15 18:08:35 +0700

    Fix(constants): Changed Server url constant to use next public server url to work in production

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,6 +1,6 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Kween Store'
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Modern ecomerce store for kween'
-export const SERVER_URL = process.env.SERVER_URL || 'http://localhost:3000'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 5
 
 export const signInDefaultValues = {
@@ -64,9 +64,20 @@ export const reviewFormDefaultValues = {
 export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 
 export const productCategories = [
-  { 'Muscle Growth': ['Pre-workout', 'Post-workout', 'Whey protein', 'Casein protein', 'Plant-based protein', 'Creatine', 'BCAAs', 'Glutamine'] },
+  {
+    'Muscle Growth': [
+      'Pre-workout',
+      'Post-workout',
+      'Whey protein',
+      'Casein protein',
+      'Plant-based protein',
+      'Creatine',
+      'BCAAs',
+      'Glutamine',
+    ],
+  },
   { 'Weight Loss': ['Fat burners', 'Appetite suppressants', 'Thermogenics'] },
-  { 'Endurance': [ 'Electrolytes', 'Hydration powders', 'Energy boosters'] },
-  { 'Food and Nutrition': ['Food supplements', 'Food bars', 'Food powders','Joint support', 'Bone strength'] },
-  { 'Health and Wellness': ['Sleep aids', 'Digestive health', 'Immunity boosters','Herbal supplements'] },
+  { Endurance: ['Electrolytes', 'Hydration powders', 'Energy boosters'] },
+  { 'Food and Nutrition': ['Food supplements', 'Food bars', 'Food powders', 'Joint support', 'Bone strength'] },
+  { 'Health and Wellness': ['Sleep aids', 'Digestive health', 'Immunity boosters', 'Herbal supplements'] },
 ]
